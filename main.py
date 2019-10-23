440a8c35719322d676d68799b794c34d3958e333
Mituz <m.tuzhikov@hotmail.de>
2019-10-23 14:57:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 train_set_path = "./DataSources/train.csv"
 train_data = pd.read_csv(train_set_path)
 
 #print(train_data.describe())
 #print(train_data.head)
-print(train_data.columns)
+#print(train_data.columns)
 
 
 
@@ -21,7 +24,41 @@ X = train_data[features_1]
 y = train_data['Survived']
 
 
-model = DecisionTreeClassifier (random_state=1)
-model.fit(X,y)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 6)
 
-print(model.predict(X))
+
+model = DecisionTreeClassifier (random_state=8)
+model.fit(train_X,train_y)
+
+
+predictions = model.predict(val_X)
+print(mean_absolute_error(val_y, predictions))
+
+#------------------- Das Gleiche mit einem RandomForrest-Classifier
+
+model = RandomForestClassifier (random_state=8)
+model.fit(train_X,train_y)
+
+
+predictions = model.predict(val_X)
+print(mean_absolute_error(val_y, predictions))
+
+#--------------------------------
+
+
+
+"""
+test_set_path = "./DataSources/test.csv"
+test_data = pd.read_csv(test_set_path)
+
+#print(train_data.describe())
+#print(train_data.head)
+print(test_data.columns)
+
+test_X = test_data['SibSp']
+test_y = test_data['Survived']
+
+predictions = model.predict(test_X)
+
+print(mean_absolute_error(test_y, predictions))
+"""
